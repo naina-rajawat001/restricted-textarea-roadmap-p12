fd37630536c17b9bca0f3999bfc9beb81a8513c4
naina-rajawat001 <naina.rajawat001@gmail.com>
2026-04-27 10:43:47 +0530

    commit

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
 const message = document.getElementById("usermsg")
 const letterCount = document.getElementById("lettercount")
 const inputBox = document.getElementById("inputbox")
-
-let count = 0
-message.addEventListener("keydown", (e)=> {
-    
-    let letter = e.key
-   
-    keyCheck(letter)
-    letterCount.innerText = `${count} / 250` 
-    if (count === 250) {
-        if (letter != "Backspace" || letter != "Delete") {
-            inputBox.classList.add("limitover")
-            message.classList.add("red")
-            e.preventDefault()
-        }
+let count = 0;
+message.addEventListener("input" , (e) => {
+    const value = e.target.value
+    count = value.length
+    letterCount.innerText = `${count} / 250`
+    if (value.length === 250) {    
+        message.classList.add("red")
+        inputBox.classList.add("limitover")
+        message.maxLength = "250"
     }
-})
 
-function keyCheck(word) {
-    if((word === "Backspace" || word === "Delete")) {
-        count > 0 ? count-- : count
-        inputBox.classList.remove("limitover")
+    if(value.length < 250 ) {
         message.classList.remove("red")
-    }else {
-        count < 250 ? count++ : count
+        inputBox.classList.remove("limitover")
     }
-}
-
-
-    
+ }) 
